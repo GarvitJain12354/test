15037f03472b014898a8dff549e5f7002b20f5f9
first <garvitjainraju@gmail.com>
2023-08-10 23:30:34 +0530

    first commit

--- script.js
+++ script.js
@@ -1,54 +1,67 @@
 var div =  gsap.utils.toArray(".box")
+
 var tl =  gsap.timeline({
     scrollTrigger:{
         trigger:"#page2",
         pin:true,
         start:"top top",
-        markers:true,
         scrub:0.5,
        end:"200% 0%"
     }
 })
 .to(".sec:nth-child(1)",{
-    y:"-100%"
+    y:"-80%"
 },"a")
-.to(".h1 h1",{
+.to(".h1 h1,.p p,.p2 p,.button button",{
     y:"-100%"
     },"a")
     .to("#o1",{
         opacity:0,
         delay:-.1
     },"a")
+    .to("#o2",{
+        opacity:1,
+        delay:-.1
+    })
 .to(".sec:nth-child(1)",{
-    y:"-200%"
+    y:"-160%"
 },"b")
-.to(".h1 h1",{
+.to(".h1 h1,.p p,.p2 p,.button button",{
     y:"-200%"
     },"b")
- 
-    .to("#o2",{
+ .to("#o2",{
         opacity:0,
         delay:-.1
     },"b")
+    .to("#o3",{
+        opacity:1,
+        delay:-.1
+    })
 .to(".sec:nth-child(1)",{
-    y:"-300%"
+    y:"-240%"
 },"c")
-.to(".h1 h1",{
+.to(".h1 h1,.p p,.p2 p,.button button",{
     y:"-300%"
     },"c")
     .to("#o3",{
         opacity:0,
         delay:-.1
     },"c")
+    .to("#o4",{
+        opacity:1,
+        delay:-.1
+    })
 .to(".sec:nth-child(1)",{
-    y:"-400%"
+    y:"-320%"
 },"d")
-.to(".h1 h1",{
+.to(".h1 h1,.p p,.p2 p,.button button",{
     y:"-400%"
     },"d")
     .to("#o4",{
         opacity:0,
         delay:-.1
     },"d")
-
-console.log("-" + 100 * div.length - 100  +  "%"  );
+    .to("#o5",{
+        opacity:1,
+        delay:-.1
+    })
